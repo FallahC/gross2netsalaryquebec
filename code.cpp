a7f63fe0f6efbdb160339f43caf92c64ce3595e7
FallahC <fallah@tuta.io>
2017-04-12 20:01:48 -0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 /*Variables*/
 
 float gross, net; 
@@ -7,28 +9,28 @@ float gross, net;
 /*Taxes will be deduced from the gross salary according to Quebec's taxable income rates.*/
 
 int main(){
-    std::cout << "Please input your yearly gross salary without spaces.\n";
-    std::cin >> gross;
+    cout << "Please input your yearly gross salary without spaces.\n";
+    cin >> gross;
     
     if (gross <= 42390)
     {
         net = gross * 0.86;
-        std::cout << "Your net salary is " << net << "$." << std::endl;
+        cout << "Your net salary is " << net << "$." << endl;
         }
     else if (gross > 42390 && gross <= 84780)
     {
         net = gross * 0.80;
-        std::cout << "Your net salary is " << net << "$." << std::endl;
+        cout << "Your net salary is " << net << "$." << endl;
         }
     else if (gross > 84780 && gross <= 103150)
     {
         net = gross * 0.76;
-        std::cout << "Your net salary is " << net << "$." << std::endl;
+        cout << "Your net salary is " << net << "$." << endl;
         }
     else if (gross > 103150)
     {
         net = gross * 0.7475;
-        std::cout << "Your net salary is " << net << "$." << std::endl;
+        cout << "Your net salary is " << net << "$." << endl;
         }
     return 0;
 }
